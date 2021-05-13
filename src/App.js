3bdca683feca23f537bda3d56ae30644591f5f60
Raphael <raphael@raphael.WLAN_AP>
2021-05-12 21:26:50 -0300

    requisicao get

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import api from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  state={
+    filmes: [],
+  }
+  async componentDidMount(){
+    const response = await api.get('superman');
+    
+    this.setState({filmes: response.data})
+  }
+
+  render(){
+    const { filmes } = this.state;
+    return(
+      <div className='App'>
+        <h1>Lista de Filmes</h1>
+        {console.log('filmes listados',filmes)}
+        <table className='tabela'> 
+          <tr>
+            <th><b> ID </b></th>
+            <th><b> Name Film </b></th>
+            <th><b> Release date </b></th>
+            <th><b> Genres </b></th>
+          </tr>
+         {filmes.map(filme => (
+          <tr>
+            <td>{filme.show.id}</td>
+            <td>{filme.show.name}</td>
+            <td>{filme.show.premiered}</td>
+            <td>{filme.show.genres[0]}</td>
+          </tr>
+       ))}
+        </table>
+      </div>
+    )
+  }
 }
 
 export default App;
